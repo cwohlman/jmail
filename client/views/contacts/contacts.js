2443c73c6421125f322490ce0f19d15bad6624c4
Joshua Ohlman <codewriterohs@gmail.com>
2014-04-25 22:53:31 -0500

    Add contacts search feature

--- client/views/contacts/contacts.js
+++ client/views/contacts/contacts.js
@@ -1,6 +1,21 @@
 Template.Contacts.helpers({
 	filteredContacts: function () {
-		return this.contacts;
+		var filter = Session.get('search');
+		var result = this.contacts.collection.find().fetch(); // Is this the correct way to preserve the reactiveness of the cursor?
+		if (filter) {
+			result = _.filter(result, function (item) {
+				// TODO: implement sublime text style search?
+				var match = false;
+				if (item.name.search(filter) != -1) match = true;
+				if (item.email.search(filter) != -1) match = true;
+				if (_.any(item.tags, function (tag) {
+					// TODO: should a tag have to match exactly?
+					return tag.search(filter) != -1
+				})) match = true;
+				return match;
+			});
+		}
+		return result;
 	}
 });
 
@@ -17,5 +32,9 @@ Template.Contacts.events({
 			email: email,
 			tags: tags
 		})
+	},
+	'keydown .search-value': function (e, tmpl) {
+		var value = e.target.value;
+		Session.set('search', value);
 	}
 });
