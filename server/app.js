82710eb94e8245bad7df2e315e41ea3afb11066a
Joshua Ohlman <codewriterohs@gmail.com>
2014-04-28 13:48:49 -0500

    Customize verify Email message

--- server/app.js
+++ server/app.js
@@ -1,4 +1,15 @@
 // extend App with server-side functions
 Accounts.config({
 	sendVerificationEmail: true
-})
+});
+
+Accounts.emailTemplates.siteName = "JMail";
+Accounts.emailTemplates.from = "JMail Admin <admin@somejmaildomain.com>";
+Accounts.emailTemplates.verifyEmail.subject = function (user) {
+    return "Welcome to JMail!";
+};
+Accounts.emailTemplates.verifyEmail.text = function (user, url) {
+   return "Thanks for trying out our cool little app! "
+     + " To activate your account, simply click the link below:\n\n"
+     + url;
+};
