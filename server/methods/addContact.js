b1430eae536ac59e1240a8b67f461b62c425305c
Joshua Ohlman <codewriterohs@gmail.com>
2014-05-01 07:37:15 -0500

    Update meteor methods to return error numbers
    addContact and sendMessage now return an error message.

--- server/methods/addContact.js
+++ server/methods/addContact.js
@@ -1,16 +1,16 @@
 Meteor.methods({
 	addContact: function (newContact) {
-		if (!Meteor.userId()) throw new Meteor.error("Only logged in users can create contacts.");
-		if (!newContact) throw new Meteor.error("Contact details missing.")
+		if (!Meteor.userId()) throw new Meteor.error(403, "Only logged in users can create contacts.");
+		if (!newContact) throw new Meteor.error(400, "Contact details missing.")
 		var name = newContact.name;
 		var email = newContact.email;
 		var tags = newContact.tags;
-		if (!name) throw new Meteor.error("All contacts must have a name");
-		if (!email) throw new Meteor.error("All contacts must have an email");
+		if (!name) throw new Meteor.error(400, "All contacts must have a name");
+		if (!email) throw new Meteor.error(400, "All contacts must have an email");
 		// TODO: Better regex? It might be better to send a confirmation email.
-		if (!/[^@]+@[^@]+/.test(email)) throw new Meteor.error("Contact email must match xx@xx");
+		if (!/[^@]+@[^@]+/.test(email)) throw new Meteor.error(400, "Contact email must match xx@xx");
 		// Better to use duck typing ie !tags.concat ?
-		if (tags && !(tags instanceof Array)) throw new Meteor.error("The tags property must be an array.");
+		if (tags && !(tags instanceof Array)) throw new Meteor.error(400, "The tags property must be an array.");
 		tags = tags || []; // we want to store an empty array if no tags specified.
 		Contacts.insert({
 			owner_id: Meteor.userId(),
--- server/methods/sendMessage.js
+++ server/methods/sendMessage.js
@@ -1,13 +1,13 @@
 Meteor.methods({
 	sendMessage: function (newMessage) {
-		if (!Meteor.userId()) throw new Meteor.Error("Only logged in users can create messages.");
-		if (!newMessage) throw new Meteor.Error("Message details missing.")
+		if (!Meteor.userId()) throw new Meteor.Error(403, "Only logged in users can create messages.");
+		if (!newMessage) throw new Meteor.Error(400, "Message details missing.")
 		var to = newMessage.to;
 		var subject = newMessage.subject;
 		var body = newMessage.body;
-		if (!to) throw new Meteor.Error("All messages must have a recipient");
-		if (!subject) throw new Meteor.Error("All messages must have a subject");
-		if (!body) throw new Meteor.Error("All messages must have a body");
+		if (!to) throw new Meteor.Error(400, "All messages must have a recipient");
+		if (!subject) throw new Meteor.Error(400, "All messages must have a subject");
+		if (!body) throw new Meteor.Error(400, "All messages must have a body");
 		var user = Meteor.user();
 		var from = user.emails[0].address;
 		Messages.insert({
