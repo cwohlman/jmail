2270fb73917a274b203d5955c4d9b00bb6e2e6fe
Joshua Ohlman <codewriterohs@gmail.com>
2014-05-01 07:05:58 -0500

    Update comments for account email verification

--- client/app.js
+++ client/app.js
@@ -47,13 +47,14 @@ _.each(Helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper)
 });
 
+// I copied this method from the accounts-ui package (or one of it's dependancies)
+// I've modified it to use my notification system
 Meteor.startup(function () {
   if (Accounts._verifyEmailToken) {
     Accounts.verifyEmail(Accounts._verifyEmailToken, function(error) {
       Accounts._enableAutoLogin();
       if (!error) App.sendNotification('success', 'Thank You for verifying your Email! You may now add contacts and send messages.');
       else App.sendNotification('error', 'Could not verify email!');
-      // XXX show something if there was an error.
     });
   }
 });
